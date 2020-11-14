12b113d30434dd3c771d0533df8c8b75f4b28cb8
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-14 19:20:38 +0900

    validationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
-  # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # 正規表現を変数設定
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  
+  with_options presence: true do
+    validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
+    validates :nickname
+  end
 end
