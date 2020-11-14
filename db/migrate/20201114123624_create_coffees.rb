cdc78a0214fa5c7f966d419323db4ceb741169c6
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-14 21:47:37 +0900

    モデルの作成

--- db/migrate/20201114123624_create_coffees.rb
+++ db/migrate/20201114123624_create_coffees.rb
@@ -1,7 +1,12 @@
 class CreateCoffees < ActiveRecord::Migration[6.0]
   def change
     create_table :coffees do |t|
-
+      t.string     :name,            null:false
+      t.integer    :country_id,      null:false
+      t.date       :purchase_date,   null:false
+      t.string     :shop,            null:false
+      t.text       :detail          
+      t.references :user,            foreign_key: true      
       t.timestamps
     end
   end
