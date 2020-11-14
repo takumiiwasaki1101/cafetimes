efad1be54df0889caa5c13fc4243cf3017bb68bf
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-14 19:52:54 +0900

    rubocopによる修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,8 +3,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   # 正規表現を変数設定
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
+
   with_options presence: true do
     validates_format_of :password, with: PASSWORD_REGEX, message: 'Include both letters and numbers'
     validates :nickname
