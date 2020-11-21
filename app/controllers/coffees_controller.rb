2bcf80a98aeaf657824556df591a4e472d5874c5
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-21 09:14:47 +0900

    createアクションを作成

--- app/controllers/coffees_controller.rb
+++ app/controllers/coffees_controller.rb
@@ -7,5 +7,23 @@ class CoffeesController < ApplicationController
   def new 
     @coffee = Coffee.new
   end
+
+  def create
+    @coffee = Coffee.new(coffee_params)
+    if @coffee.valid?
+      @coffee.save  # バリデーションをクリアした時
+      redirect_to root_path
+    else
+      render action: 'new' # バリデーションに弾かれた時
+    end
+
+  end
+
+  private
   
+  def coffee_params
+    params.require(:coffee).permit(:name, :country_id, :date_of_purchase, :shop, :detail).merge(user_id: current_user.id)
+  end
+
 end
+
