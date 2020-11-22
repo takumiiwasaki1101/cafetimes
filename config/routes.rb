ae2ef89579272a8c05ebf4f338e65cc2cea9d160
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-22 15:45:28 +0900

    ルーティングにネストを導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root to: "toppages#index"
   devise_for :users
-  resources :coffees, only: [:index, :new, :create, :edit, :update, :destroy]
-  resources :reviews, only: [:index, :new, :create]
+  resources :coffees, only: [:index, :new, :create, :edit, :update, :destroy] do
+    resources :reviews, only: [:new, :create]
+  end
+  resources :reviews, only:[:index]
 end
