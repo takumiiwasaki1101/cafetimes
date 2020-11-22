a322a26e8a0428b35bea80f491f40303b0d8a05a
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-22 17:33:12 +0900

    params情報の修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -23,7 +23,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:date, :text).merge(user_id: current_user.id, coffee_id: params[:coffee_id])
+    params.require(:review).permit(:date, :review).merge(user_id: current_user.id, coffee_id: params[:coffee_id])
   end
 
 end
