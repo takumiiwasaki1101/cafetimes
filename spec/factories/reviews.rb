6639f32df22496b7bf68ac657e31c935837cf7cc
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-23 16:57:12 +0900

    テストコードの作成

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :review do
-    
+    date { Faker::Date.in_date_period }
+    review { Faker::Lorem.sentence }
+    association :user    
+    association :coffee    
   end
 end
--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -1,5 +1,31 @@
 require 'rails_helper'
 
 RSpec.describe Review, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe '#create' do
+    before do
+      @review = FactoryBot.build(:review)
+    end
+
+    context '新規登録成功時' do
+      it 'date、reviewが存在すれば登録できること' do
+        expect(@review).to be_valid
+      end
+    end
+
+    context '新規登録失敗時' do
+      # 空では登録できないことを確認
+      it 'dateが空では登録できないこと' do
+        @review.date = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Date can't be blank")
+      end
+
+      it 'reviewが空では登録できないこと' do
+        @review.review = nil
+        @review.valid?
+        expect(@review.errors.full_messages).to include("Review can't be blank")
+      end
+      # 空では登録できないことを確認
+    end
+  end
 end
