1e43f97181e7fc2663bcf639bc87b394787320d4
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-24 23:26:44 +0900

    リンクの設定（初回レビュー時の円滑な操作を実現

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
   def show
     @coffees = Coffee.where(user_id: current_user.id).order('created_at DESC')
+    @coffee  = Coffee.where(user_id: current_user.id).order('created_at DESC').first
     @reviews = Review.where(user_id: current_user.id).order('created_at DESC')
   end
 end
