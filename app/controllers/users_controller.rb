8e8c1349ea2c3e77c624d86fff1f329dadba3057
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-28 18:25:56 +0900

    コントローラーの変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,8 +6,9 @@ class UsersController < ApplicationController
   end
 
   def show
-    @coffees = Coffee.where(user_id: current_user.id).order('created_at DESC')
-    @coffee  = Coffee.where(user_id: current_user.id).order('created_at DESC').first
-    @reviews = Review.where(user_id: current_user.id).order('created_at DESC')
+    @user = User.find(params[:id])
+    @coffees = Coffee.where(user_id: @user.id)
+    @coffee  = Coffee.where(user_id: @user.id).order('created_at DESC').first
+    @reviews = Review.where(user_id: @user.id).includes(:coffee).order('created_at DESC')
   end
 end
