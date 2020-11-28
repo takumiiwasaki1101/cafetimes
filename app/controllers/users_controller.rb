7a823b845db7ee02557ae90eea2e946fd3b2e52d
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-28 14:27:32 +0900

    コントローラーの作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
   before_action :authenticate_user!
 
+  def index
+    @users = User.all.order('created_at DESC')
+  end
+
   def show
     @coffees = Coffee.where(user_id: current_user.id).order('created_at DESC')
     @coffee  = Coffee.where(user_id: current_user.id).order('created_at DESC').first
     @reviews = Review.where(user_id: current_user.id).order('created_at DESC')
   end
+  
 end
