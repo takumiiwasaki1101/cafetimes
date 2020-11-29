08e0eae6b21cf66f8bb17bc934cb5b83ab16d072
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-29 18:29:43 +0900

    paramsで受け取る値の範囲を修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -25,7 +25,7 @@ class ReviewsController < ApplicationController
   private
 
   def review_params
-    params.require(:review).permit(:date, :tool_id, :review).merge(user_id: current_user.id, coffee_id: params[:coffee_id])
+    params.require(:review).permit(:date, :tool_id, :amount_of_coffee, :amount_of_water, :pre_infusion_time, :extraction_time, :review).merge(user_id: current_user.id, coffee_id: params[:coffee_id])
   end
 
   def set_coffee
