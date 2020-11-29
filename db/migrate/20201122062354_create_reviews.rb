7776afbcb593f0d404b072dd67800f5871059a05
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-29 15:56:58 +0900

    reviewモデルのカラム追加

--- db/migrate/20201122062354_create_reviews.rb
+++ db/migrate/20201122062354_create_reviews.rb
@@ -3,7 +3,11 @@ class CreateReviews < ActiveRecord::Migration[6.0]
     create_table :reviews do |t|
       t.date       :date,    null:false
       t.integer    :tool_id, null:false
-      t.text       :review          
+      t.integer    :amount_of_coffee
+      t.integer    :amount_of_water 
+      t.integer    :pre_infusion_time 
+      t.integer    :extraction_time 
+      t.text       :review,  null:false          
       t.references :user,    foreign_key: true      
       t.references :coffee,  foreign_key: true      
       t.timestamps
