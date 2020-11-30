500089bc82f1ae82d6f08008d8b427020d09b898
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-11-30 21:08:46 +0900

    sign_inメソッドの導入

---
+++ spec/support/sign_in_support.rb
@@ -0,0 +1,9 @@
+module SignInSupport
+  def sign_in(user)
+    visit new_user_session_path
+    fill_in 'email', with: @user.email
+    fill_in 'password', with: @user.password
+    click_on("commit")
+    expect(current_path).to eq coffees_path
+  end
+end
