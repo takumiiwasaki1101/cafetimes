4401863c6c75797f92981106bf78085ac0f843b9
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-12-01 22:27:04 +0900

    編集機能のRspec

--- spec/system/coffees_spec.rb
+++ spec/system/coffees_spec.rb
@@ -67,3 +67,52 @@ RSpec.describe "コーヒー豆の登録機能", type: :system do
     end
   end
 end
+
+RSpec.describe "コーヒー豆の編集機能", type: :system do
+  before do
+    # コーヒーをDBに保存する
+    @coffee = FactoryBot.create(:coffee)
+  end
+
+  context '編集に成功したとき' do
+    it 'ログインしたユーザーは自分が登録したコーヒーの編集ができる' do
+      # コーヒーを登録したユーザーでサインインする
+      sign_in(@coffee.user)
+      # 登録したコーヒーの画像内に「編集」ボタンがあることを確認する
+      expect(
+        all(".more")[0].hover
+      ).to have_link '編集', href: edit_coffee_path(@coffee)
+ 
+      # コーヒー豆の編集ページへ遷移する
+      visit edit_coffee_path(@coffee)
+
+      # すでに登録済みの内容がフォームに入っていることを確認する
+      expect(
+        find('#name').value
+      ).to eq @coffee.name
+      expect(
+        find('#country').value
+      ).to eq @coffee.country.id.to_s
+      expect(
+        find('#coffee_date_of_purchase_1i').value
+      ).to eq @coffee.date_of_purchase
+
+      select '2020', from: "coffee_date_of_purchase_1i" 
+      select '12', from: "coffee_date_of_purchase_2i" 
+      select '10', from: "coffee_date_of_purchase_3i" 
+      fill_in 'shop', with: @shop
+      fill_in 'detail', with: @detail
+
+      # コーヒー豆の登録をクリックすると、Coffeeモデルのカウントが1上がることを確認する
+      expect{
+        find('input[name="commit"]').click
+      }.to change { Coffee.count }.by(1)
+
+      # コーヒー豆の一覧ページにリダイレクトされることを確認する
+      expect(current_path).to eq coffees_path
+
+      # 一覧ページに先ほどの登録内容が含まれていることを確認する
+      expect(page).to have_content @name
+    end
+  end
+end
