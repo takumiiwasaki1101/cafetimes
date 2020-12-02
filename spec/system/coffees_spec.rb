053bbd72b1dd7024ce0d754e092de7c5a90b46c8
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-12-02 20:52:22 +0900

    コーヒーモデルの編集・削除テスト

--- spec/system/coffees_spec.rb
+++ spec/system/coffees_spec.rb
@@ -4,6 +4,7 @@ RSpec.describe "コーヒー豆の登録機能", type: :system do
   before do
     # ユーザーをDBに保存する
     @user = FactoryBot.create(:user)
+
     # フォームに入力する情報をインスタンス変数に代入
     @name = Faker::Coffee.country
     @shop = Faker::Restaurant.name
@@ -40,6 +41,7 @@ RSpec.describe "コーヒー豆の登録機能", type: :system do
 
       # 一覧ページに先ほどの登録内容が含まれていることを確認する
       expect(page).to have_content @name
+      expect(page).to have_content @shop
     end
   end
 
@@ -54,7 +56,7 @@ RSpec.describe "コーヒー豆の登録機能", type: :system do
       # コーヒー豆の登録ページに遷移していることを確認する
       expect(current_path).to eq new_coffee_path
 
-      # コーヒー豆の登録をクリックしても、Coffeeモデルのカウントは上がらない
+      # 必須フォームに入力をしていない状態で、コーヒー豆の登録をクリックしてもCoffeeモデルのカウントは上がらない
       expect{
         find('input[name="commit"]').click
       }.not_to change { Coffee.count }
@@ -72,12 +74,18 @@ RSpec.describe "コーヒー豆の編集機能", type: :system do
   before do
     # コーヒーをDBに保存する
     @coffee = FactoryBot.create(:coffee)
+
+    # 編集用情報をインスタンス変数に代入
+    @name = Faker::Coffee.country
+    @shop = Faker::Restaurant.name
+    @detail = Faker::Lorem.sentence
   end
 
   context '編集に成功したとき' do
-    it 'ログインしたユーザーは自分が登録したコーヒーの編集ができる' do
+    it 'ログインしたユーザーは自分が登録したコーヒー情報の編集ができる' do
       # コーヒーを登録したユーザーでサインインする
       sign_in(@coffee.user)
+
       # 登録したコーヒーの画像内に「編集」ボタンがあることを確認する
       expect(
         all(".more")[0].hover
@@ -95,24 +103,66 @@ RSpec.describe "コーヒー豆の編集機能", type: :system do
       ).to eq @coffee.country.id.to_s
       expect(
         find('#coffee_date_of_purchase_1i').value
-      ).to eq @coffee.date_of_purchase
+      ).to eq @coffee.date_of_purchase.year.to_s
+      expect(
+        find('#coffee_date_of_purchase_2i').value
+      ).to eq @coffee.date_of_purchase.month.to_s
+      expect(
+        find('#coffee_date_of_purchase_3i').value
+      ).to eq @coffee.date_of_purchase.day.to_s
+      expect(
+        find('#shop').value
+      ).to eq @coffee.shop
+      expect(
+        find('#detail').value
+      ).to eq @coffee.detail
 
-      select '2020', from: "coffee_date_of_purchase_1i" 
-      select '12', from: "coffee_date_of_purchase_2i" 
-      select '10', from: "coffee_date_of_purchase_3i" 
+      # 登録内容を編集する
+      fill_in 'name', with: @name
       fill_in 'shop', with: @shop
       fill_in 'detail', with: @detail
 
-      # コーヒー豆の登録をクリックすると、Coffeeモデルのカウントが1上がることを確認する
+      # 編集してもCoffeeモデルのカウントは変わらないことを確認する
       expect{
         find('input[name="commit"]').click
-      }.to change { Coffee.count }.by(1)
+      }.to change { Coffee.count }.by(0)
 
-      # コーヒー豆の一覧ページにリダイレクトされることを確認する
+      # コーヒー一覧ページに遷移したことを確認する
       expect(current_path).to eq coffees_path
 
-      # 一覧ページに先ほどの登録内容が含まれていることを確認する
+      # 一覧ページに先ほどの変更内容が含まれていることを確認する
       expect(page).to have_content @name
+      expect(page).to have_content @shop
+    end
+  end
+end
+
+RSpec.describe 'コーヒー豆の削除機能', type: :system do
+  before do
+    # コーヒーをDBに保存する
+    @coffee = FactoryBot.create(:coffee)
+  end
+  context '削除に成功したとき' do
+    it 'ログインしたユーザーは自分が登録したコーヒー情報の削除ができる' do
+      # コーヒーを登録したユーザーでサインインする
+      sign_in(@coffee.user)
+
+      # 登録したコーヒーの画像内に「編集」ボタンがあることを確認する
+      expect(
+        all(".more")[0].hover
+      ).to have_link '削除', href: coffee_path(@coffee)
+ 
+      # 登録を削除するとレコードの数が1減ることを確認する
+      expect{
+        all(".more")[0].hover.find_link('削除', href: coffee_path(@coffee)).click
+      }.to change { Coffee.count }.by(-1)
+
+      # コーヒー一覧ページに遷移したことを確認する
+      expect(current_path).to eq coffees_path
+
+      # コーヒー一覧ページには@coffeeの内容が存在しないことを確認する
+      expect(page).to have_no_content @coffee.name
+      expect(page).to have_no_content @coffee.shop
     end
   end
 end
