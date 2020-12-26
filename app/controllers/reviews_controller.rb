1d0e79b589fea710e2394af79135fb99aff32c8d
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-12-26 21:01:44 +0900

    rubocopによる修正

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,5 +1,5 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :authenticate_user!, except: [:index, :show]
   before_action :set_coffee, only: [:new, :create]
   before_action :move_to_coffees_path, only: [:new, :create]
 
