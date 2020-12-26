e5dc7b9f5aa132a958b237b00081a629685ed755
takumiiwasaki1101 <lcmwanol@gmail.com>
2020-12-26 20:53:47 +0900

    ルーティングの設定

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   root to: "toppages#index"
   devise_for :users
   resources :coffees do
-    resources :reviews, only: [:new, :create, :edit, :update]
+    resources :reviews, only: [:new, :create, :edit, :update, :destroy]
   end
   resources :reviews, only:[:index,:show]
   resources :users, only: [:index, :show]
