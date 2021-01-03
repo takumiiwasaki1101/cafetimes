43ac2be20b03dade21065dc3573cc500dbf8daef
takumiiwasaki1101 <lcmwanol@gmail.com>
2021-01-03 18:45:18 +0900

    Update reviews.rb

--- spec/factories/reviews.rb
+++ spec/factories/reviews.rb
@@ -1,7 +1,7 @@
 FactoryBot.define do
   factory :review do
     date { Faker::Date.in_date_period }
-    tool_id { Faker::Number.within(range: 1..11) }
+    tool_id { Faker::Number.within(range: 1..10) }
     amount_of_coffee { Faker::Number.within(range: 1..100) }
     amount_of_water { Faker::Number.within(range: 100..900) }
     pre_infusion_time { Faker::Number.within(range: 10..120) }
