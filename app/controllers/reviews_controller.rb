d1f2adba46dc6c5985feddc19a8b95ff5c3a6a7f
takumiiwasaki1101 <lcmwanol@gmail.com>
2021-01-03 18:08:14 +0900

    コントローラーのリファクタリング

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,14 +1,24 @@
 class ReviewsController < ApplicationController
-  before_action :authenticate_user!, except: [:index, :show]
-  before_action :set_coffee, only: [:new, :create]
-  before_action :move_to_coffees_path, only: [:new, :create]
+
+  # リダイレクト処理
+    before_action :authenticate_user!, except: [:index, :show]
+  # //リダイレクト処理
+
+  # インスタンス変数の取得
+    before_action :set_coffee, only: [:new, :create]
+    before_action :set_review, only: [:show, :edit, :update, :destroy]
+  # //インスタンス変数の取得
+  
+  # リダイレクト処理
+    before_action :move_to_coffees_path, only: [:new, :create]
+    before_action :move_to_user_path, only: [:edit, :update, :destroy]
+  # //リダイレクト処理
 
   def index
     @reviews = Review.includes(:coffee, :user).order('date DESC')
   end
 
   def show
-    @review = Review.find(params[:id])
   end
 
   def new
@@ -26,12 +36,10 @@ class ReviewsController < ApplicationController
   end
 
   def edit
-    @review = Review.find(params[:id])
     @coffee = Coffee.find(@review.coffee.id)
   end
 
   def update
-    @review = Review.find(params[:id])
     @coffee = Coffee.find(@review.coffee.id)
     if @review.update(update_params)
       redirect_to user_path(current_user.id)
@@ -41,13 +49,13 @@ class ReviewsController < ApplicationController
   end
 
   def destroy
-    @review = Review.find(params[:id])
     @coffee = Coffee.find(@review.coffee.id)
     redirect_to user_path(current_user.id) if @review.destroy
   end
 
   private
 
+  # paramsの取得・結合
   def review_params
     params.require(:review).permit(:date, :tool_id, :amount_of_coffee, :amount_of_water, :pre_infusion_time, :extraction_time, :review).merge(user_id: current_user.id, coffee_id: params[:coffee_id])
   end
@@ -55,12 +63,27 @@ class ReviewsController < ApplicationController
   def update_params
     params.require(:review).permit(:date, :tool_id, :amount_of_coffee, :amount_of_water, :pre_infusion_time, :extraction_time, :review).merge(user_id: current_user.id, coffee_id: @coffee.id)
   end
+  # //paramsの取得・結合
 
+  # インスタンス変数の取得
   def set_coffee
     @coffee = Coffee.find(params[:coffee_id])
   end
 
+  def set_review
+    @review = Review.find(params[:id])
+  end
+  # //インスタンス変数の取得
+
+  # リダイレクト処理
   def move_to_coffees_path
     redirect_to coffees_path unless current_user.id == @coffee.user_id
   end
+
+  def move_to_user_path
+    redirect_to user_path(current_user.id) unless current_user.id == @review.user_id
+  end
+  # //リダイレクト処理
+
+
 end
